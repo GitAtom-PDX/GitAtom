dfd7dc5eeabf34a196ad38152b07611fd2c42607
jmcarlson0320 <57571031+jmcarlson0320@users.noreply.github.com>
2020-12-02 15:34:28 -0800

    Update publish.py
    added comments

--- prototype/publish/publish.py
+++ prototype/publish/publish.py
@@ -5,14 +5,21 @@ from pathlib import Path
 TARGET_DIRECTORY = "./site/posts/"
 ERROR = -1
 
+# input: string representation of path to source file
+# returns: ERROR if the source file does not exist
 def publish(path_to_src):
     src_path = Path(path_to_src)
     if not src_path.exists():
         return ERROR
 
+    # extract the filename from full path, split filname into pieces
+    # based on '-'
+    # ex: 2020-12-1-post.html is split into:
+    # ['2020', '12', '1', 'post.html']
     src_filename = src_path.name
     name_tokens = src_filename.split("-")
 
+    # build the destination directory based on previous pieces
     dest_path = Path(TARGET_DIRECTORY)
     for i in range(len(name_tokens) - 1):
         dest_path = dest_path / name_tokens[i]
@@ -20,11 +27,13 @@ def publish(path_to_src):
     if not dest_path.exists():
         dest_path.mkdir(parents = True)
 
+    # append filename w/out date info to destination path
     dest_path = dest_path / name_tokens[-1]
 
     shutil.copy(src_path, dest_path)
     return
 
+# this script just takes in a file name as a command line argument, then publishes it
 def main():
     if len(sys.argv) != 2:
         print("file not specified")
