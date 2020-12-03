4491c30c3c3133df8728f8b7fc49ae176a2605e1
Kenbo0601 <kenichi2@pdx.edu>
2020-12-03 15:40:15 -0800

    modified jinja script

--- prototype/jinja/prototype/script.py
+++ prototype/jinja/prototype/script.py
@@ -1,14 +1,29 @@
 from jinja2 import Environment, FileSystemLoader
 from xml.dom import minidom
 
-#get data from xml
-mydoc = minidom.parse('test.xml')
-home = mydoc.getElementsByTagName('home')[0]
-skills = mydoc.getElementsByTagName('skills')
-#print(home.firstChild.data)
-s = skills[1].getAttribute("name")
-print(s)
+# get data from xml
+mydoc = minidom.parse('input.xml')
+f_title = mydoc.getElementsByTagName('title')[0]
+f_updated = mydoc.getElementsByTagName('updated')[0]
+f_id = mydoc.getElementsByTagName('id')[0]
+entry = mydoc.getElementsByTagName('entry')
+# print(home.firstChild.data)
 
+list = []
+
+for i in entry:
+    title = i.getElementsByTagName("title")[0]
+    id = i.getElementsByTagName("id")[0]
+    published = i.getElementsByTagName("published")[0]
+    updated = i.getElementsByTagName("updated")[0]
+
+    # save data into list
+    list.append(title.firstChild.data)
+    list.append(id.firstChild.data)
+    list.append(published.firstChild.data)
+    list.append(updated.firstChild.data)
+
+print(list)
 
 # load layout html file
 template_env = Environment(loader=FileSystemLoader(searchpath='./'))
@@ -17,7 +32,7 @@ template = template_env.get_template('layout.html')
 with open('sample.html', 'w') as outfile:
     outfile.write(
         template.render(
-            title=s,
+            title=list[0],
             date='11/27/2020',
             blog='this is a test blog post.'
         )
