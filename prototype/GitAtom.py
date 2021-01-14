1b4792e5538ed1c763e05d97b5111079931189a9
Walker <bearwalker93@gmail.com>
2021-01-13 20:58:25 -0800

    Changed pipeline to store md in atom feed instead of HTML

--- prototype/GitAtom.py
+++ prototype/GitAtom.py
@@ -24,37 +24,28 @@ class Atom:
         self.files = [None] * 4
         self.author = ''
         self.date = datetime.datetime.now()
+   
+    #Place new md to atom code here ####################
 
-    # funtion to convert markdown file into html
-    # called by mdtohtml('filename.md')
-    # either returns a html file on success or None on failure
-    def mdtohtml(self, md_file):
-        # make sure there is a valid os path
-        if os.path.exists(md_file):
-            html_name = os.path.basename(md_file)
-            title = len(html_name)
 
-            # make sure file is a .md markdown file
-            if html_name[title - 3:title] == '.md':
-                html_name = html_name[:title - 3]
-                md_file = open(md_file, "r")
-                md_text = md_file.read()
-                md_file.close()
+    ####################################################
 
-                html_text = cmarkgfm.markdown_to_html(md_text)
-                # TODO need to change naming convention of new html files
-                html_file = open('{0}.html'.format(html_name), "w")
-                html_file.write(html_text)
-                html_file.close()
-                return html_file.name  # success
-        return None  # failure
+    def md_to_atom(self, md_filename):
 
-    def html_to_atomify(self, html_filename):
-                # Open required files
-        html = html_filename
+        #use if we want to change to reading in from command arg
+        """   
+        # Open required files - this is currently designed such 
+        # that atomify [file] processes one md file at a time
+        if not len(sys.argv) == 2: exit("Usage: python3 atomify.py input.md")
+        if not sys.argv[1].endswith('.md'): exit("Incorrect input file type (expected .md)")
+        md = sys.argv[1]
+        """
+
+        md = md_filename
+
 
         # NOTE may not need this if using a separate file for feed tags...
-        config_f = open('./atomify/gitatom.config')
+        config_f = open('../gitatom.config')
         config = config_f.readlines()
         config_f.close()
 
@@ -62,12 +53,16 @@ class Atom:
         feed_id = config[0].strip()
         feed_title = config[1].strip()
 
-        entry_title = os.path.splitext(os.path.basename(html))[0]
+        entry_title = os.path.splitext(os.path.basename(md))[0] # TODO make os-agnostic 
         entry_id = feed_id + entry_title # depends on feed id
 
-        entry_published = datetime.datetime.now() # using current time
-        entry_updated = entry_published
-        feed_updated = entry_updated # depends on entry updated
+        # TODO how to check if the given markdown file is a new or existing post...
+        # how best to handle updating an existing post? 
+
+        entry_published = datetime.datetime.now()		# using current time
+        entry_published.replace(microsecond=0) 	# truncate ms
+        entry_updated = entry_published			# TODO how to handle updating entries...?
+        feed_updated = entry_updated 		# depends on entry updated
 
         # Create atom string
         atom = '<feed>\n'
@@ -80,20 +75,42 @@ class Atom:
         atom += '<published>' + str(entry_published) + '</published>\n'
         atom += '<updated>' + str(entry_updated) + '</updated>\n'
         atom += '<content>' 
-        with open (html,'r') as f: 
-            atom += f.read()
+
+        # NOTE https://stackoverflow.com/questions/3411771/best-way-to-replace-multiple-characters-in-a-string
+        with open (md,'r') as f: 
+            atom += f.read().replace('<', '\<').replace('>', '\>')
+            
         atom += '</content>\n'
         atom += '</entry>\n'
         atom += '</feed>\n'
 
         # Write result to file
-        outname = entry_title + '.xml' # NOTE need a good naming schema...
+        outname = entry_title + '.xml' # TODO need a good naming schema...
         outfile = open(outname, 'w')
         outfile.write(atom)
         outfile.close()
-        return outfile.name
+        return outname
+
+    # funtion to convert markdown text into html
+    # called by md_to_html(md_text,'filename.xml')
+    # either returns a html filename on success or None on failure
+    def md_to_html(self, md_text, xml_filename):
+
+        l = len(xml_filename)
+        
+        # make sure file is a .xml file
+        if xml_filename[l - 4:l] == '.xml':
+            html_name = xml_filename[:l - 4]
+            html_text = cmarkgfm.markdown_to_html(md_text)
+            # TODO need to change naming convention of new html files
+            html_file = open('{0}.html'.format(html_name), "w")
+            html_file.write(html_text)
+            html_file.close()
+            return html_file.name  # success
+        return None  # failure 
 
     # Jinja - html generator
+    #TODO need to add error checking and clean up this function 
     def render_html(self, xml_filename):
         # get data from xml
         #mydoc = minidom.parse('../../atomify/input.xml')
@@ -103,6 +120,9 @@ class Atom:
         f_id = mydoc.getElementsByTagName('id')[0]
         entry = mydoc.getElementsByTagName('entry')
         content = mydoc.getElementsByTagName('content')
+
+        html_name = self.md_to_html(content[0].firstChild.nodeValue,xml_filename)
+        self.files[2] = html_name
 
         list1 = []
         
@@ -118,11 +138,14 @@ class Atom:
             list1.append(published.firstChild.data)
             list1.append(updated.firstChild.data)
 
+        #TODO this loop breaks the function, need to check indexing or find another way to do this
+        """
         # getting data from content
         for i in content:
             blog = i.getElementsByTagName("p")[0]
             list1.append(blog.childNodes)
-       
+        """
+
         # load template html file
         template_env = Environment(
             loader=FileSystemLoader(searchpath='./jinja/templates'))
@@ -130,7 +153,7 @@ class Atom:
 
         
         # Open and read the html file for it's content
-        html_file = open(self.files[1], "r")
+        html_file = open(html_name, "r")
         html_text = html_file.read()
         html_file.close()
         
@@ -151,9 +174,10 @@ class Atom:
     # the file. Example: aaa-bbb-ccc-file.html is copied to
     # ./site/posts/aaa/bbb/ccc/file.html
 
-    def publish(self, path_to_src):
+    #TODO work on file saving directory structure
+    def save_atom(self, path_to_src):
 
-        TARGET_DIRECTORY = "./site/posts/"
+        TARGET_DIRECTORY = "../site/posts/"
         ERROR = -1
 
         src_path = Path(path_to_src)
@@ -200,14 +224,14 @@ def main(markdown_file):
 
     atom.files[0] = markdown_file
     print(atom.files[0])
-    atom.files[1] = atom.mdtohtml(atom.files[0])
+    atom.files[1] = atom.md_to_atom(atom.files[0])   #  ------------new md to atom call goes here with same arg---------------
     print(atom.files[1])
-    atom.files[2] = atom.html_to_atomify(atom.files[1])
-    print(atom.files[2])
-    atom.render_html(atom.files[2])
+    #atom.files[2] = atom.html_to_atomify(atom.files[1])
+    #print(atom.files[2])
+    atom.render_html(atom.files[1])
 
-
-    if not atom.publish(atom.files[2]):
+    #files[2] is assigned in th render_html() call after parsing the content out of the xml file
+    if not atom.save_atom(atom.files[2]):
         sys.exit('Fail')
 
     # repo = Repo('/Users/Uwriyel/PycharmProjects/GitAtom')
@@ -226,7 +250,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
-        main(args.markdown_file)
+        main(args.markdown_file)        #can be used later to feed md files through command line
 
     except KeyboardInterrupt:
         print('***** Finished GitAtom *****')
