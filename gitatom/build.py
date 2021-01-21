5a99d11b154121acfbcda5d71bded380d192289b
Bruh Beyene <bbeyene@pdx.edu>
2021-01-21 00:12:27 -0800

    author, title use in index page ip

--- gitatom/build.py
+++ gitatom/build.py
@@ -12,7 +12,7 @@ def create(publish_directory):
     print(f"creating {publish_directory}/index.html and {publish_directory}/style.css")
 
     html = BeautifulSoup(f"""<html lang="en"><head><link rel=stylesheet type=text/css href="{publish_directory}/style.css">
-	<title>Blog Post List</title></head><body><div id=main></div></body></html>""", features='html.parser')
+	<title>Blog Post List</title></head><body><header><h1></h1><h2></h2></header><div id=main></div></body></html>""", features='html.parser')
     css = "* { box-sizing: 'content-box'; }"
 
     with open(publish_directory + '/index.html', "w") as index:
@@ -26,6 +26,8 @@ def append(filename):
 
     # get target directory from config file 
     publish_directory = config.options['publish_directory']
+    author = config.options['author']
+    feed_title = config.options['feed_title']
 
     # parse publish_directory/index.html
     index_file = publish_directory + '/index.html'
@@ -35,18 +37,30 @@ def append(filename):
     # parse filename (html post) DOM for title and link
     with open(f'{publish_directory}/posts/' + filename, 'r') as f:
         post = BeautifulSoup(f, 'html.parser')
+    # what it should be: post_title = post.html.head.title.string
+    post_title = post.html.body.center.h1.string
+    # what it should be: post_date = post.html.h2.string
+    post_date = post.html.body.center.next_sibling.next_sibling.h3.string
     post_link = 'posts/' + ''.join(filename.split('/')[-1:])
-    post_title = ''.join(filename.split('.')[-2:-1])
-    site_title = post.title.string
 
     # insert blog details 
     new_link = index.new_tag('a', href=post_link)
     new_link.string = post_title
+
+    new_p = index.new_tag('p')
+    new_p.string = post_date
+    new_p.insert(0, new_link)
+
     new_div = index.new_tag('div')
+    new_div.insert(0, new_p)
     new_div['class'] = 'entry'
-    new_div.insert(0, new_link)
 
     index.html.body.insert(0, new_div)
+
+    index.html.head.title.string = feed_title
+    index.html.body.header.h1 = feed_title
+    index.html.body.header.h2 = author
+
     with open(index_file, 'w') as f:
         f.write(index.prettify())
 
