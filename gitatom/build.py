4a2f133827c0035f685bd91e80b2c3f0a9ad2b14
Bruh Beyene <bbeyene@pdx.edu>
2021-01-29 09:01:42 -0800

    cleaned up code and sidebar view

--- gitatom/build.py
+++ gitatom/build.py
@@ -1,10 +1,11 @@
-# given: 'main_templates/base.html', 
-#        'main_templates/default.html',
+# given: 'main_templates/nav.html', 
+#        'main_templates/blog.html',
+#        'main_templates/archive.html',
 #        'publish_directory/*.html'
 #        'publish_directory/posts/*.html'
 #
-# build.py: scans publish_directory and publish_directory/posts/
-#           renders the base and default templates
+# build.py: scans publish_directory, atoms/ and publish_directory/posts/
+#           renders the nav, blog and archive templates
 #
 # output:   publish_directory/index.html
 
@@ -47,39 +48,37 @@ def build_it():
     for atom in atoms:
         tree = ET.parse(atom)
         root = tree.getroot()
+        content = root.find('entry').find('content').text
         post = dict()
         post['title'] = root.find('entry').find('title').text
         post['updated'] = root.find('entry').find('updated').text
-        content = root.find('entry').find('content').text
         post['body'] = cmarkgfm.markdown_to_html(content)
         post['link'] = 'posts/' + atom.stem + '.html'
         posts.append(post)
-        sorted_posts = sorted(posts, key=lambda post: post['updated'], reverse=True)
         archive.append( { 'title' : post['title'], 'link' : post['link'], 'updated' : post['updated'] } )
+        sorted_posts = sorted(posts, key=lambda post: post['updated'], reverse=True)
         sorted_archive = sorted(archive, key=lambda item: item['updated'], reverse=True)
 
-    sidebar_len = len(sorted_archive) if len(sorted_archive) < 5 else 5
 
-    # render blog template with found entries and other pages
+    # render blog and archive templates
     file_loader = FileSystemLoader('gitatom/main_templates/')
     env = Environment(loader=file_loader)
-    template = env.get_template('blog-a.html')
-    rendered_blog = template.render(title=site_title, author=site_author, \
-                                    nav=nav_dict, posts=sorted_posts, \
-                                    sidebar=sorted_archive[0:sidebar_len])
+    sidebar_len = len(archive) if len(archive) < 5 else 5
 
-    # render archive template with found entries and other pages
-    file_loader = FileSystemLoader('gitatom/main_templates/')
-    env = Environment(loader=file_loader)
-    template = env.get_template('archive-a.html')
-    rendered_archive = template.render(title=site_title, author=site_author, \
-                                        nav=nav_dict, archive=sorted_archive)
+    temp1 = env.get_template('blog-a.html')
+    rendered_blog = temp1.render(title=site_title, author=site_author, \
+                                 nav=nav_dict, posts=sorted_posts, \
+                                 sidebar=sorted_archive[0:sidebar_len])
+
+    temp2 = env.get_template('archive-a.html')
+    rendered_archive = temp2.render(title=site_title, author=site_author, \
+                                    nav=nav_dict, archive=sorted_archive)
 
     # write the html
     index = site_dir / "index.html"
-    archive = site_dir / "archive.html"
     with open(index, 'w') as f:
         f.write(rendered_blog)
+    archive = site_dir / "archive.html"
     with open(archive, 'w') as f:
         f.write(rendered_archive)
 
